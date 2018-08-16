f79ca8e0c43bdbb1c4368ce071b7a30d4e68dda1
inno1103 <42234902+inno1103@users.noreply.github.com>
2018-08-16 15:26:36 +0800

    Update nodejs.js

--- nodejs.js
+++ nodejs.js
@@ -9,7 +9,7 @@ const regtest = bitcoin.networks.testnet
 const bip65 = require('bip65')
 
 //輸入私鑰，教學用，請勿隨意公開私鑰
-var privateKey = "cNcvQphXWjAJ365Y8Tuhti5a6fCh9ftpR3GScZs5vdidBwQqpxT4";
+var privateKey = "cSzQrcp2zoihasXYMAtkF6zwVG5875eERdQzaXAmwED9G1o1N7tK";
 
 //產生公鑰跟私鑰
 const keyPair = bitcoin.ECPair.fromWIF(privateKey,regtest);
